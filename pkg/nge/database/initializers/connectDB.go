547467eea156b5b78990e6a089d4fad5a5baf508
Baisal <baisalkenesbek40@gmail.com>
2024-05-09 22:17:57 +0500

    commit

--- pkg/nge/database/initializers/connectDB.go
+++ pkg/nge/database/initializers/connectDB.go
@@ -12,16 +12,14 @@ var DB *gorm.DB
 
 func ConnectDB(config *Config) {
 	var err error
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", config.DBHost, config.DBUserName, config.DBUserPassword, config.DBName, config.DBPort)
-
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	externalURL := "postgres://nge_db_user:gvDhoCCHguRmZ7EhNpXtuH69sKlv6sIZ@dpg-cos9h7nsc6pc73e2rcc0-a.singapore-postgres.render.com/nge_db"
+	DB, err = gorm.Open(postgres.Open(externalURL), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to the Database")
 	}
-	fmt.Println("? Connected Successfully to the Database")
+	fmt.Println("Connected Successfully to the Database")
 }
 
-func GetDB() *gorm.DB{
+func GetDB() *gorm.DB {
 	return DB
 }
-
